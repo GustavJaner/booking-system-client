aebf7da959d95925d564e8379aca47a10bf12038
ChristianOrmos <ormos@live.se>
2019-12-02 16:22:11 +0100

    UI fix, navbar etc

---
+++ src/components/Navbar/navbar.js
@@ -0,0 +1,180 @@
+import React from 'react';
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import MeetingRoomIcon from '@material-ui/icons/MeetingRoom';
+import VpnKeyIcon from '@material-ui/icons/VpnKey';
+import LocalLaundryServiceIcon from '@material-ui/icons/LocalLaundryService';
+import MailIcon from '@material-ui/icons/Mail';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  menuButton: {
+    marginRight: 36,
+  },
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+    whiteSpace: 'nowrap',
+  },
+  drawerOpen: {
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerClose: {
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: theme.spacing(7) + 1,
+    [theme.breakpoints.up('sm')]: {
+      width: theme.spacing(9) + 1,
+    },
+  },
+  toolbar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+}));
+
+export default function MiniDrawer() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  const handleId = (e) => {
+    console.log(e.target.id);
+    console.log(e.currentTarget.id);
+  }
+
+  return (
+    <>
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, {
+              [classes.hide]: open,
+            })}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Admin
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant="permanent"
+        className={clsx(classes.drawer, {
+          [classes.drawerOpen]: open,
+          [classes.drawerClose]: !open,
+        })}
+        classes={{
+          paper: clsx({
+            [classes.drawerOpen]: open,
+            [classes.drawerClose]: !open,
+          }),
+        }}
+        open={open}
+      >
+        <div className={classes.toolbar}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+          </IconButton>
+        </div>
+        <Divider />
+        <List>
+
+          <ListItem button key={"Services"} onClick={() => { console.log('Services') }}>
+            <ListItemIcon ><LocalLaundryServiceIcon/></ListItemIcon>
+            <ListItemText primary={"Services"} />
+          </ListItem>
+
+          <ListItem button key={"Rooms"} onClick={() => { console.log('Rooms') }}>
+            <ListItemIcon ><MeetingRoomIcon/></ListItemIcon>
+            <ListItemText primary={"Rooms"} />
+          </ListItem>
+
+          <ListItem button key={"AccessGroups"} onClick={() => { console.log('Access Groups') }}>
+            <ListItemIcon ><VpnKeyIcon/></ListItemIcon>
+            <ListItemText primary={"Access Groups"} />
+          </ListItem>
+
+        </List>
+        <Divider />
+      </Drawer>
+
+    </>
+  );
+}
+
+/*{['Services', 'Rooms', 'Access Groups', 'Mail'].map((text, index) => (
+            <ListItem button key={text} onClick={() => { console.log('onClick') }}>
+              <ListItemIcon >{[<LocalLaundryServiceIcon/>, <MeetingRoomIcon/>, <VpnKeyIcon />, <MailIcon/>][index]}</ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}*/
--- src/containers/Admin/index.js
+++ src/containers/Admin/index.js
@@ -9,6 +9,20 @@ import useRooms from "../../components/Rooms/useRooms"
 import CreateRoomForm from "../../components/Room/CreateRoomForm"
 import AccessGroupList from "../../components/AccessGroups/List"
 import useAccessGroups from "../../components/AccessGroups/useAccessGroups"
+import Navbar from "../../components/Navbar/navbar"
+import { makeStyles, useTheme } from '@material-ui/core/styles'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    marginTop: theme.spacing(6)
+  },
+}));
 
 const Admin = () => {
   const { services = [], loading } = useServices()
@@ -17,13 +31,23 @@ const Admin = () => {
   const servicesVisible = useVisible()
   const roomsVisible = useVisible()
   const accessGroupVisible = useVisible()
+  const classes = useStyles();
+  const theme = useTheme();
 
   //console.log(accessGroups)
   //console.log(servicesVisible)
+
+
   return (
+    
     <MuiThemeProvider>
-      <>
-        <h1> Admin</h1>
+      <div className={classes.root}> 
+
+      <Navbar />
+      
+      <div className={classes.content}>
+
+      
         <h2 onClick={() => servicesVisible.toggle()}>Services</h2>
         {servicesVisible.open && (
           <>
@@ -45,7 +69,9 @@ const Admin = () => {
         {accessGroupVisible.open && (
           <AccessGroupList accessGroups={accessGroups} />
         )}
-      </>
+        </div>
+      </div>
+
     </MuiThemeProvider>
   )
 }
